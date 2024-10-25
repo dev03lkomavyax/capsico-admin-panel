93935fa97fffffd83b8ca7e94d25001f5d8f8549
dev05mavyax <dev05@mavyax.com>
2024-10-26 00:12:32 +0530

    code updated

---
+++ src/components/component/order/Quickly.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quickly = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Quickly
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,7 +53,12 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
+  		},
+		fontFamily:{
+			roboto:"Roboto",
+			sans:"Open Sans",
+			inter:"Inter",
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
