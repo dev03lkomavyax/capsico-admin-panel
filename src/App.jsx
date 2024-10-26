007e7876e478fee130a88723a747783ff5662ee9
dev03lkomavyax <dev03lko@mavyax.com>
2024-10-26 19:25:42 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Suspense fallback={<div className='w-full h-screen bg-white text-black flex justify-center items-center text-xl font-semibold'>Loading...</div>}>
         <Routes>
           <Route path='/admin/order' element={<Order />} />
-          <Route path='/admin/order/123' element={<OrderDetails />} />
+          <Route path='/admin/order/:orderId' element={<OrderDetails />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </Suspense>
