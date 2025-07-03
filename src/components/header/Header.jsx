37611b145d1608c8bd1e596ac2042491f8477c2b
dev03lkomavyax <dev03lko@mavyax.com>
2025-07-03 12:07:00 +0530

    code updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,75 +9,78 @@ import AlertModal from "../AlertModal";
 import Spinner from "../Spinner";
 
 const Header = () => {
-    const userInfo = readCookie("userInfo");
-    const navigate = useNavigate();
+  const userInfo = readCookie("userInfo");
+  const navigate = useNavigate();
 
-    const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
-    const { res, fetchData, isLoading } = usePostApiReq();
+  const { res, fetchData, isLoading } = usePostApiReq();
 
-    console.log("userInfo", userInfo);
-    
+  console.log("userInfo", userInfo);
 
-    const logout = () => {
-        fetchData(`/admin/logout-all`,{role:userInfo.role || "admin",phone:userInfo.phone});
-    };
+  const logout = () => {
+    fetchData(`/admin/logout-all`, {
+      role: userInfo?.role || "admin",
+      phone: userInfo?.phone,
+    });
+  };
 
-    useEffect(() => {
-        if (res?.status === 200 || res?.status === 201) {
-            console.log("logout res", res);
-            localStorage.removeItem("admin-status");
-            navigate("/");
-            setIsLogoutModalOpen(false);
-        }
-    }, [res, setIsLogoutModalOpen]);
+  useEffect(() => {
+    if (res?.status === 200 || res?.status === 201) {
+      console.log("logout res", res);
+      localStorage.removeItem("admin-status");
+      navigate("/");
+      setIsLogoutModalOpen(false);
+    }
+  }, [res, setIsLogoutModalOpen]);
 
-    return (
-        <header className="h-20 bg-white ps-4 pe-10 py-5 flex justify-end items-center">
-            {/* <div className='flex items-center gap-6'>
+  return (
+    <header className="h-20 bg-white ps-4 pe-10 py-5 flex justify-end items-center">
+      {/* <div className='flex items-center gap-6'>
                 <img src={logo} className='w-[51px] h-[48px]' alt="de9to" />
                 <img src={logo} className='w-[132px] h-[48px]' alt="de9to" />
             </div> */}
-            <div className="flex items-center gap-4">
-                <FiBell className="text-lg text-[#397FFE]" />
-                <FiSettings className="text-lg text-[#397FFE]" />
+      <div className="flex items-center gap-4">
+        <FiBell className="text-lg text-[#397FFE]" />
+        <FiSettings className="text-lg text-[#397FFE]" />
 
-                {!userInfo?.userImage ? (
-                    <button
-                        onClick={() => navigate("/admin/sub-admin/update-admin-profile")}
-                        className="w-14 cursor-pointer h-14 rounded-full bg-gray-100 flex justify-center items-center"
-                    >
-                        <FaUser className="text-3xl text-gray-500" />
-                    </button>
-                ) : (
-                    <img
-                        onClick={() => navigate("/admin/sub-admin/update-admin-profile")}
-                        src={userInfo?.userImage}
-                        className="object-cover border p-[1px] w-14 h-14 rounded-full cursor-pointer"
-                        alt="profile-img"
-                    />
-                )}
-                {isLoading ?
-                    <Spinner size={20} className="w-auto text-gray-500" /> :
-                    <LogOutIcon
-                        className="text-gray-500 cursor-pointer"
-                        onClick={() => setIsLogoutModalOpen(true)}
-                    />
-                }
-            </div>
+        {!userInfo?.userImage ? (
+          <button
+            onClick={() => navigate("/admin/sub-admin/update-admin-profile")}
+            className="w-14 cursor-pointer h-14 rounded-full bg-gray-100 flex justify-center items-center"
+          >
+            <FaUser className="text-3xl text-gray-500" />
+          </button>
+        ) : (
+          <img
+            onClick={() => navigate("/admin/sub-admin/update-admin-profile")}
+            src={userInfo?.userImage}
+            className="object-cover border p-[1px] w-14 h-14 rounded-full cursor-pointer"
+            alt="profile-img"
+          />
+        )}
+        {isLoading ? (
+          <Spinner size={20} className="w-auto text-gray-500" />
+        ) : (
+          <LogOutIcon
+            className="text-gray-500 cursor-pointer"
+            onClick={() => setIsLogoutModalOpen(true)}
+          />
+        )}
+      </div>
 
-            {isLogoutModalOpen && (
-                <AlertModal
-                    isAlertModalOpen={isLogoutModalOpen}
-                    setIsAlertModalOpen={setIsLogoutModalOpen}
-                    header="Logout"
-                    description="Are you sure you want to logout?"
-                    onConfirm={logout}
-                    disabled={isLoading}
-                />
-            )}
-        </header>
-    );
+      {isLogoutModalOpen && (
+        <AlertModal
+          isAlertModalOpen={isLogoutModalOpen}
+          setIsAlertModalOpen={setIsLogoutModalOpen}
+          header="Logout"
+          description="Are you sure you want to logout?"
+          onConfirm={logout}
+          disabled={isLoading}
+        />
+      )}
+    </header>
+  );
 };
 
 export default Header;
